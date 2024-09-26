b6af12afcb415cda58749e79aaf1b0eff8f22b3c
Karam Sawalha <89216965+ksawalha@users.noreply.github.com>
2024-09-27 00:26:08 +0300

    Update screenshot.js

--- www/screenshot.js
+++ www/screenshot.js
@@ -4,4 +4,8 @@ exports.takeScreenshot = function(success, error) {
     exec(success, error, 'ScreenshotPlugin', 'takeScreenshot', []);
 };
 
+exports.connectPrinter = function(escPosCommands, success, error) {
+    exec(success, error, 'ScreenshotPlugin', 'connectPrinter', [escPosCommands]);
+};
+
 module.exports = exports;
